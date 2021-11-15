ac8872382f40b9a5f2e1c5e3c245c0dd7fb6093a
Dylan Nunns <dylan@gnarledrootsystems.com>
2021-11-15 01:26:45 -0400

    Work work

--- app/src/main/java/com/gnarledrootsystems/onedayto/MainActivity.kt
+++ app/src/main/java/com/gnarledrootsystems/onedayto/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.gnarledrootsystems.onedayto
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
+import androidx.core.view.children
 import androidx.navigation.Navigation
 import com.gnarledrootsystems.onedayto.databinding.ActivityMainBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,12 +18,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setSupportActionBar(findViewById(R.id.toolbar))
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         Navigation.findNavController(this, R.id.nav_host_fragment)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        var formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy")
+        val date = LocalDate.parse("$year-$month-$day")
+        val formatted_date = date.format(formatter)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "OneDay.to"
+        actionBar.subtitle = formatted_date
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
@@ -28,6 +54,18 @@ class MainActivity : AppCompatActivity() {
 
         R.id.action_calendar -> {
 
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in TextView
+                //textView.setText("" + dayOfMonth + " " + month + ", " + year)
+                val date = "" + dayOfMonth + " " + month + ", " + year
+            }, year, month, day)
+            dpd.show()
+
             true
         }
 
@@ -36,4 +74,5 @@ class MainActivity : AppCompatActivity() {
             super.onOptionsItemSelected(item)
         }
     }
+
 }
